fc20c040dd21edb46445cb6ea843549823e36aea
nedeauberndsen693 <nedeauberndsen693@gmail.com>
2025-03-15 04:02:08 +0000

    Add 10.java

---
+++ src/10.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+public class GitHubSchoolProject {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int randomNumber = r.nextInt(10);
+        System.out.println("Random number: " + randomNumber);
+    }
+}
